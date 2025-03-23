412701e423a3d18e0e80be134701adb345329bec
Ross Fish <moffenzeef_studio@Rosss-Mac-mini.local>
2025-03-23 11:58:24 -0700

    improved LED response and added CV label to input

--- src/BadIdea9.cpp
+++ src/BadIdea9.cpp
@@ -35,7 +35,7 @@ struct BadIdea9 : Module {
 			config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 			configParam(RUH_PARAM, 0.0f, 1.0f, 0.5f, "Main Oscillator Frequency", " Hz", 11.781f, 160.f); 
 			configParam(ROH_PARAM, 0.0f, 1.0f, 0.5f, "Sync Oscillator Frequency", " Hz", 300.f, 10.f); 
-			configInput(PWR_INPUT, "Voltage Starve");
+			configInput(PWR_INPUT, "Voltage Starve CV");
 			configOutput(AUDIO_OUTPUT, "Bad Idea #9");
 	
 		}
@@ -110,7 +110,7 @@ struct BadIdea9 : Module {
 			float LED = cvInput * 0.2f;
 			
 			outputs[AUDIO_OUTPUT].setVoltage(finalOutput);
-			lights[LED_LIGHT].setBrightness(LED);
+			lights[LED_LIGHT].setBrightnessSmooth(LED, args.sampleTime);
 		}					
 	};
 		
