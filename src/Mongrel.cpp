cf19e3cc194e3833fa3ecd47316724fbf370f434
Ross Fish <moffenzeefmodular@gmail.com>
2025-08-06 17:18:46 -0700

    Mongrel: dark panels implemented

--- src/Mongrel.cpp
+++ src/Mongrel.cpp
@@ -162,7 +162,10 @@ void process(const ProcessArgs& args) override {
 struct MongrelWidget : ModuleWidget {
 	MongrelWidget(Mongrel* module) {
 		setModule(module);
-		setPanel(createPanel(asset::plugin(pluginInstance, "res/Mongrel.svg")));
+	setPanel(createPanel(
+		asset::plugin(pluginInstance, "res/Mongrel.svg"),
+		asset::plugin(pluginInstance, "res/Mongrel-dark.svg")
+		));
 
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
